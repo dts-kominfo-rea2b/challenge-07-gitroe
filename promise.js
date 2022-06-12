5fa786eb09e7ed70929296e3e6d0a37ec819ea90
gitroe <aingtehheru@outlook.com>
2022-06-12 23:50:17 +0700

    update challenge Promise
    mengupdate TODO sesuai tantangan

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (reaksi) => {
+  try {
+    const [iix, vgc] = await Promise.all([promiseTheaterIXX(),promiseTheaterVGC()]);
+    let jumlah = 0; //harus let karena nilainya berubah terus hingga berhenti
+    jumlah += iix?.filter((film) => film.hasil === reaksi)?.length || 0;
+    jumlah += vgc?.filter((film) => film.hasil === reaksi)?.length || 0;
+    return jumlah;
+  } 
+  catch (error) {
+    console.log("Pesan error: " + error);
+  }
+};
 
 module.exports = {
   promiseOutput,
